c05a2741d83ec75d74054aac7ad5e9a3a28b2d12
Hao Shan <bhsh0112@163.com>
2026-01-05 11:29:32 +0800

    modify some parameter

--- scripts/train_moe.sh
+++ scripts/train_moe.sh
@@ -1,10 +1,11 @@
 python $NAVSIM_DEVKIT_ROOT/navsim/planning/script/run_training.py \
   agent=diffusiondrive_agent \
-  experiment_name=training_diffusiondrive_agent_layerwise_moe_no_noise_traj_router \
+  experiment_name=training_diffusiondrive_agent_layerwise_moe_lb_2e2 \
   train_test_split=navtrain \
   split=trainval \
   trainer.params.max_epochs=100 \
   trainer.params.strategy=ddp_find_unused_parameters_true \
   cache_path="${NAVSIM_EXP_ROOT}/training_cache/" \
   use_cache_without_dataset=True \
-  force_cache_computation=False
+  force_cache_computation=False \
+  +agent.config.moe_load_balance_coef=2e-2
